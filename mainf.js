d9810d6d73ac4f56ee7836ec09aa18f6e5fd6f64
Віталій Палюшок < >
2016-08-15 11:55:09 +0300

    updated .js file

--- mainf.js
+++ mainf.js
@@ -1,9 +1,9 @@
-var totalWinSum = 0;
-var maxNumberOfRow = 5;
-var firstRound10 = 10;
-var firstRound5 = 5;
-var firstRound2 = 2;
-var firstRound0 = 0;
+	var totalWinSum = 0;
+	var maxNumberOfRow = 5;
+	var firstRound10 = 10;
+	var firstRound5 = 5;
+	var firstRound2 = 2;
+	var firstRound0 = 0;
 
 	while ( confirm ( "Would you like to play the game?" ) ) {
 		alert ( "Let\'s start" );
@@ -11,96 +11,87 @@ var firstRound0 = 0;
 	}
 
 	if( totalWinSum==0 ){
-
 		alert( 'You didn\'t win the billion' );
+	}	else{
+
+			alert( "YOUR FINAL PRIZE IS: " + totalWinSum + " $" );
 	}
-
-	else{
-
-		alert( "YOUR FINAL PRIZE IS: " + totalWinSum + " $" );
-}
 
 
 	function play() { 
 		
-	var tries = 3;
-	var enteredNumber = Math.round( Math.random() * 5 ); 
+		var tries = 3;
+		var enteredNumber = Math.round( Math.random() * 5 ); 
 		
 
-	for ( var i = 0; i < 3; i++ ) {
+		for ( var i = 0; i < 3; i++ ) {
 
-		alert( "You have: " + tries + " tries" );
+			alert( "You have: " + tries + " tries" );
 
-		//alert( enteredNumber ); //для правильного введення
+			//alert( enteredNumber ); //для правильного введення
 
-		var userNumber = prompt( "Choose the number from 0 to " + maxNumberOfRow, "");
-		
-		var isUserNumberCorrect = userNumber == enteredNumber; 
-
+			var userNumber = prompt( "Choose the number from 0 to " + maxNumberOfRow, "");
 			
-		if(isUserNumberCorrect) {
-			
-			if (tries == 3) {
+			var isUserNumberCorrect = userNumber == enteredNumber; 
 
-				totalWinSum += firstRound10;
-				maxNumberOfRow *= 2;
+				
+			if(isUserNumberCorrect) {
+				
+				if (tries == 3) {
 
-				alert( "You won " + firstRound10 + "$" );
+					totalWinSum += firstRound10;
+					maxNumberOfRow *= 2;
 
-				alert( "Your balance is: " + totalWinSum + " $" );
+					alert( "You won " + firstRound10 + "$" );
 
-				firstRound10 *= 3;
-				break;
+					alert( "Your balance is: " + totalWinSum + " $" );
+
+					firstRound10 *= 3;
+					break;
+
+				}	else if ( tries == 2 ) {
+
+						totalWinSum += firstRound5;
+						maxNumberOfRow *= 2;
+
+						alert( "You won " + firstRound5 + "$" );
+
+						alert( "You have" + totalWinSum + " $" );
+						
+						firstRound5 *= 3;
+						break;
+
+				}	else if (tries==1){
+				
+						totalWinSum += firstRound2;
+						maxNumberOfRow*=2;
+
+						alert( "You won " + firstRound2 + "$" );
+
+						alert( "You have" + totalWinSum + " $" );
+
+						firstRound2 *= 3;
+						break;
+
+				}	else {
+
+					alert( "you lost one try" );
+				
+				}
 
 			}
 
-			else if ( tries == 2 ) {
+			tries--;
 
-				totalWinSum += firstRound5;
-				maxNumberOfRow *= 2;
+			if ( tries == 0 ) {
+				
+				totalWinSum += firstRound0;
 
-				alert( "You won " + firstRound5 + "$" );
+				alert( "You won " + firstRound0 + " $" );
 
 				alert( "You have" + totalWinSum + " $" );
-				
-				firstRound5 *= 3;
+
 				break;
-
 			}
-
-			else if (tries==1){
-			
-				totalWinSum += firstRound2;
-				maxNumberOfRow*=2;
-
-				alert("You won " + firstRound2 + "$");
-
-				alert("You have" + totalWinSum + " $");
-
-				firstRound2 *= 3;
-				break;
-
-		      }
-
-			else {
-
-				alert( "you lost one try" );
-			
-			}
-
-		}
-
-		tries--;
-
-		if ( tries == 0 ) {
-			
-			totalWinSum += firstRound0;
-
-			alert( "You won " + firstRound0 + " $" );
-
-			alert( "You have" + totalWinSum + " $" );
-
-			break;
 		}
 	}
-}
